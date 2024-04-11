739e91bc9ff06cc23bc01a21106d3b9d0818bc11
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-04-11 12:18:50 +0530

    added custom api response and errors

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,15 +1,14 @@
 import { registerBody } from "../utils/types.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiError } from "../utils/ApiErrror.js";
 
 const registerUser = async (req, res) => {
   //get input from the user/frontend
   //validate the inputs
   const { success } = registerBody.safeParse(req.body);
   if (!success) {
-    return res.status(400).json({
-      message: "Email already taken / Incorrect credentials",
-    });
+    throw new ApiError(400, "Invalid credentials")
   }
 
   //   check if user already exists
@@ -20,21 +19,13 @@ const registerUser = async (req, res) => {
   });
 
   if (existingUser) {
-    return res.status(400).json({
-      message: "User with this email or username already exists",
-    });
+    throw new ApiError(400, "User with this email or username already exists")
   }
 
   //   check for profilePicture
   const profilePictureLocalPath = req.files?.profilePicture[0]?.path;
 
   const profilePicture = await uploadOnCloudinary(profilePictureLocalPath);
-
-  if (!profilePicture) {
-    return res.status(400).json({
-      message: "profile picture is required",
-    });
-  }
 
   const user = await User.create({
     username,
@@ -48,14 +39,12 @@ const registerUser = async (req, res) => {
   const createdUser = await User.findById(user._id).select("-password");
 
   if (!createdUser) {
-    return res
-      .status(500)
-      .json({ message: "Something went wrong while creating the user" });
+   throw new ApiError(500, "Something went wrong while creating the user")
   }
 
-  return res.status(201).json({
-    message: "User registered succesfully",
-  });
+  return res.status(201).json(
+    new ApiResponse(201, createdUser, "User registered succesfully"),
+  );
 };
 
 export { registerUser };
