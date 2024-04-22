dcddee836ed487e89e65d16de4e0c7b31553d8fc
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-04-23 01:17:57 +0530

    added inputs based on signup/signin

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import Input from "../components/Input";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
-  const [isSigIn, setIsSignIn] = useState(false);
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleSignup = () => {
+    setIsSignIn(!isSignIn);
+  };
 
   return (
     <div className="bg-slate-900 flex-grow flex  justify-center">
@@ -18,17 +22,41 @@ const SignIn = () => {
           Flats N Flatmates
         </h1>
         <Input placeholder="Enter Username" icon={faUser} />
-        <Input placeholder="Enter Password" type="password" icon={faLock} />
+        {!isSignIn && <Input placeholder="Enter Full Name" icon={faUser} />}
+        {!isSignIn && (
+          <Input placeholder="Enter email" type="email" icon={faEnvelope} />
+        )}
+        {!isSignIn && (
+          <Input placeholder="Phone Number" type="tel" icon={faPhone} />
+        )}
+
+        <Input placeholder="Password" type="password" icon={faLock} />
+        {!isSignIn && (
+          <Input placeholder="Confirm Password" type="password" icon={faLock} />
+        )}
         <Button
-          children={"SignIn"}
+          children={isSignIn ? "Login" : "Sign Up"}
           className="w-full mt-4 flex justify-center"
         />
-        <p className="my-4">
-          Don’t you have an account?
-          <span className="mx-2 text-indigo-700 hover:text-indigo-500">
-            <Link to="/signup">Sign up</Link>{" "}
-          </span>
-        </p>
+        {isSignIn ? (
+          <p className="my-4">
+            Don’t you have an account?
+            <span className="mx-2 text-indigo-700 hover:text-indigo-500">
+              <Link to="/signup" onClick={toggleSignup}>
+                Sign up
+              </Link>
+            </span>
+          </p>
+        ) : (
+          <p className="my-4">
+            Already have an account?
+            <span className="mx-2 text-indigo-700 hover:text-indigo-500">
+              <Link to="/signin" onClick={toggleSignup}>
+                Sign in
+              </Link>
+            </span>
+          </p>
+        )}
       </div>
     </div>
   );
