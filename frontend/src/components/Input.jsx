98785585a65a82d0bd3fb1041c1822106796d425
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-05-04 17:29:17 +0530

    added form validation to signin page

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Input = React.forwardRef(
-  ({ placeholder, type = "text", icon, className = "", ...props }, ref) => {
-    return (
-      <div className={`relative w-4/5 m-4 ${className}`}>
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <FontAwesomeIcon icon={icon} className="text-indigo-700" />
-        </div>
-        <input
-          placeholder={placeholder}
-          type={type}
-          className="pl-10 w-full py-4 pr-3 text-white bg-gray-900 border-2 border-white rounded-lg"
-          {...props}
-          ref={ref}
-        />
+const Input = ({
+  placeholder,
+  type = "text",
+  icon,
+  inputRef,
+  className = "",
+  ...props
+}) => {
+  return (
+    <div className={`relative w-4/5 m-4 ${className}`}>
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <FontAwesomeIcon icon={icon} className="text-indigo-700" />
       </div>
-    );
-  }
-);
+      <input
+        placeholder={placeholder}
+        ref={inputRef}
+        type={type}
+        className="pl-10 w-full py-4 pr-3 text-white bg-gray-900 border-2 border-white rounded-lg"
+        {...props}
+      />
+    </div>
+  );
+};
 
 export default Input;
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Input from "../components/Input";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faLock, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import Button from "../components/Button";
 import { Link } from "react-router-dom";
+import { checkValidateData } from "../utils/validate";
 
 const SignIn = () => {
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // validate the form data
+    const message = checkValidateData(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(message);
+    // send backend request to /login
+  };
+
   return (
     <div className="bg-slate-900 flex  justify-center">
       <div className=" bg-gray-950 md:w-2/5 w-full text-white border-2 border-white rounded-xl py-7 px-2 my-20 flex flex-col items-center shadow-2xl shadow-indigo-700">
@@ -15,12 +32,20 @@ const SignIn = () => {
         <h1 className="font-medium text-4xl p-2  text-indigo-700 text-center">
           Flats N Flatmates
         </h1>
-        <Input placeholder="Enter Username" icon={faUser} />
-        <Input placeholder="Password" type="password" icon={faLock} />
-        <Button
-          children={"Login"}
-          className="w-full mt-4 flex justify-center"
-        />
+        <form
+          className="w-full flex flex-col items-center "
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <Input inputRef={email} placeholder="Enter Username" icon={faUser} />
+          <Input
+            inputRef={password}
+            placeholder="Password"
+            type="password"
+            icon={faLock}
+          />
+          <p className="text-red-500 font-bold text-lg">{errorMessage}</p>
+          <Button children={"Login"} className="mt-4" type="submit" />
+        </form>
         <p className="my-4">
           Don’t have an account?
           <span className="mx-2 text-indigo-700 hover:text-indigo-500">
