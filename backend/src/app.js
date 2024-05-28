e4f81c9ebd3ec89abbdd7b4b97234ef73c0b1855
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-05-28 20:39:15 +0530

    aded render url for cors and deleted vercel.json

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://flats-and-flatmates.vercel.app/",
+    ],
     credentials: true,
   })
 );
