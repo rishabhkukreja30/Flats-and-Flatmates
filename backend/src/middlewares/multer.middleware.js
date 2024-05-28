916cdc028de96d1fb7dd883adb3d9b8b9c359ea6
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-05-28 19:23:20 +0530

    corrected file path

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,8 +1,12 @@
 import multer from "multer";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    const filePath = path.join(__dirname, "public/temp");
+    cb(null, filePath);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
